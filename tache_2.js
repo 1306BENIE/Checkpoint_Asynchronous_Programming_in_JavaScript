54829205805d4f45d9e2c58e43f96f9ceec6da2f
BENIE SYLVESTRE <sylvestrebenie7829@gmail.com>
2025-03-13 17:00:35 +0000

    feat: tache_2

--- tache_2.js
+++ tache_2.js
@@ -1,1 +1,10 @@
-8
+async function parallelCalls(urls) {
+    try {
+        const responses = await Promise.all(
+            urls.map(url => fetch(url).then(response => response.json()))
+        );
+        console.log('Réponses de toutes les requêtes parallèles :', responses);
+    } catch (error) {
+        console.error('Erreur lors des appels parallèles :', error.message);
+    }
+}
